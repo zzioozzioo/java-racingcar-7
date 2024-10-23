a183e58dbcb865ddc8d9636a79cee1c10439770f
zzioozzioo <zzioo1006@naver.com>
2024-10-24 00:15:47 +0900

    test: 자동차 이름, 시도 횟수 입력 예외 처리에 대한 테스트 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -7,6 +7,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -28,6 +29,49 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름이_5자_초과인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("qwerty,abc", "2"))
+                        .isInstanceOf(IllegalArgumentException.class));
+
+    }
+
+    @Test
+    void 자동차_이름을_1개만_입력한_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("qwert", "2"))
+                        .isInstanceOf(IllegalArgumentException.class));
+
+    }
+
+    @Test
+    void 쉼표와_쉼표_사이에_이름을_입력하지_않은_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("qwe,,abc", "3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 이름에_공백이_있는_경우() {
+        assertSimpleTest(() -> {
+            run("a , b,c", "2");
+            assertThat(output()).contains("a : ");
+            assertThat(output()).contains("b : ");
+            assertThat(output()).contains("c : ");
+        });
+    }
+
+    @Test
+    void 시도할_횟수가_1_미만이거나_100_초과인_경우() {
+        assertAll(
+                () -> assertThatThrownBy(() -> runException("qwe, rty", "0"))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> runException("qwe, rty", "101"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
