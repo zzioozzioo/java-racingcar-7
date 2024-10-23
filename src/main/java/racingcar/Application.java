b91541c5f4ecc727fa9332377f4b5587c18930dc
zzioozzioo <zzioo1006@naver.com>
2024-10-24 00:16:23 +0900

    feat: 자동차 이름, 시도 횟수 입력 예외 처리 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.*;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,11 +14,27 @@ public class Application {
         // TODO: 프로그램 구현
 
         final String DELIMITER = ",";
+        final int MIN_TRY_COUNT = 1;
+        final int MAX_TRY_COUNT = 100; // 수정 가능
 
         int inputCount = 0;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCarName = Console.readLine().trim();
+        String inputCarName = Console.readLine();
+
+        if (!inputCarName.contains(DELIMITER)) {
+            throw new IllegalArgumentException("자동차의 이름은 두 개 이상 입력해야 합니다.");
+        }
+
+        String[] splitCarName = Arrays.stream(inputCarName.split(DELIMITER, -1))
+                .map(String::trim)
+                .toArray(String[]::new);
+
+        for (String carName : splitCarName) {
+            if (carName.isEmpty()) {
+                throw new IllegalArgumentException("쉼표와 쉼표 사이에 이름을 입력해야 합니다.");
+            }
+        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         try {
@@ -26,9 +43,13 @@ public class Application {
             throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
         }
 
-        String[] splitCarName = inputCarName.split(DELIMITER);
+        if (inputCount < MIN_TRY_COUNT || inputCount > MAX_TRY_COUNT) {
+            throw new IllegalArgumentException("시도할 횟수는 1 이상 100 이하여야 합니다.");
+        }
+
         Map<String, Integer> racingCar = new HashMap<>();
         for (String s : splitCarName) {
+            validateNameLength(s);
             racingCar.put(s, 0);
         }
 
@@ -45,11 +66,18 @@ public class Application {
 
     }
 
+    private static void validateNameLength(String s) {
+        if (s.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+        }
+    }
+
     private static boolean checkMoveOrNot() {
         int randomNum = Randoms.pickNumberInRange(0, 9);
         return randomNum >= 4;
     }
 
+    // TODO: 우승자 계산을 잘못하는 경우 발생 -> fix하기
     private static void printGameResult(Map<String, Integer> racingCar, Set<String> winningCars) {
         int winningScore = Collections.max(racingCar.values());
         for (Entry<String, Integer> carEntry : racingCar.entrySet()) {
@@ -69,6 +97,6 @@ public class Application {
 
     private static void printWinningCars(Set<String> winningCars) {
         System.out.print("최종 우승자 : ");
-        System.out.println(String.join(",", winningCars));
+        System.out.println(String.join(", ", winningCars));
     }
 }
