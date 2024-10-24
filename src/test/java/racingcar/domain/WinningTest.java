7a3ff1ef517bf8bdbe3c5dec010c70f81c4d2076
zzioozzioo <zzioo1006@naver.com>
2024-10-24 19:47:24 +0900

    test: Winning 도메인을 분리함으로써 WinningTest 추가

---
+++ src/test/java/racingcar/domain/WinningTest.java
@@ -0,0 +1,39 @@
+package racingcar.domain;
+
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class WinningTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+
+    @Test
+    void 우승자_계산_테스트() {
+        //given
+        String[] splitCarName = {"a", "b", "c"};
+        RacingCars racingCars = new RacingCars();
+        Map<String, Integer> cars = racingCars.initializeCars(splitCarName);
+
+        Winning winning = new Winning();
+        Set<String> expectedWinningCars = Set.of("a", "c");
+
+        //when % then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    racingCars.race();
+                    winning.calculateWinningScore(cars);
+
+                    Set<String> actualWinningCars = winning.getWinningCars(cars);
+                    assertThat(actualWinningCars).isEqualTo(expectedWinningCars);
+                }
+                , MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+}
