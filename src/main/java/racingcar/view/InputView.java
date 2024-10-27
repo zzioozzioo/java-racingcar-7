a0e3edae33fa611a834480d8648c51d13f7f00a9
zzioozzioo <zzioo1006@naver.com>
2024-10-27 12:41:05 +0900

    refactor(InputView): 입력에서 NoSuchElementException이 터질 수 있으므로 try-catch문을 사용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,13 +3,18 @@ package racingcar.view;
 import static racingcar.util.Delimiter.COMMA;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 import racingcar.exception.NonNumericException;
 
 public class InputView {
 
     public String readCarNames() {
         System.out.println(String.format("경주할 자동차 이름을 입력하세요.(이름은 쉼표(%s) 기준으로 구분)", COMMA.getDelimiter()));
-        return Console.readLine();
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
     }
 
     public int readTryCount() {
